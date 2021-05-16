1a8c13c2bc1251b5abb3a098b0c575bb4857d39d
Supawat Tamsri <meep@supawat.dev>
2021-05-16 21:07:05 +0200

    fixed volume tracing bugs

--- tests/volume_test.py
+++ tests/volume_test.py
@@ -18,10 +18,11 @@ while my_tracer.is_outdoor(txPos) is not True:
              np.random.random()*200-100]
 
 start = time.time()
-data_frame_1 = my_tracer.get_traced_volume(x_min=-150, x_max=150, x_n=25,
-                                           y_min=1, y_max=5, y_n=10,
-                                           z_min=-150, z_max=150, z_n=25,
+data_frame_1 = my_tracer.get_traced_volume(x_min=-150, x_max=150, x_n=10,
+                                           y_min=1, y_max=50, y_n=10,
+                                           z_min=-150, z_max=150, z_n=10,
                                            tx_pos=txPos, tx_freq=2.4e9, mat_perm=5.3,
                                            max_thread=64)
 end = time.time()
 print(f"The multithread tracing engine takes {end-start:.2f} seconds")
+print(data_frame_1.head)
